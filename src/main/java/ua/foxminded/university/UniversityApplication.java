dafae9b55eb8c75de422f342d59576bc1563e28f
Bohdan Bilyk <bohdanbilyk1001@gmail.com>
2023-07-26 19:56:21 +0200

    Removed extra one field

--- src/main/java/ua/foxminded/university/UniversityApplication.java
+++ src/main/java/ua/foxminded/university/UniversityApplication.java
@@ -1,16 +1,11 @@
 package ua.foxminded.university;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ua.foxminded.university.service.StudentService;
 
 @SpringBootApplication
 public class UniversityApplication implements CommandLineRunner {
-    @Autowired
-    StudentService studentService;
-
     public static void main(String[] args) {
 	SpringApplication.run(UniversityApplication.class, args);
     }
