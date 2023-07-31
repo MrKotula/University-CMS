05a7916dca2d6605d19d7599d710fb2efcf03bb0
Bohdan Bilyk <bohdanbilyk1001@gmail.com>
2023-07-31 17:22:30 +0200

    Changed constructor and validation

--- src/main/java/ua/foxminded/university/service/impl/UserAccountServiceImpl.java
+++ src/main/java/ua/foxminded/university/service/impl/UserAccountServiceImpl.java
@@ -28,17 +28,22 @@ public class UserAccountServiceImpl implements UserAccountService {
 
     @Override
     public void register(UserRegistrationRequest userRegistrationRequest) throws ValidationException {
-        userValidator.validateLogin(userRegistrationRequest.getEmail().trim());
-        userValidator.validateData(userRegistrationRequest.getEmail().trim(), userRegistrationRequest.getFirstName().trim(), userRegistrationRequest.getLastName().trim());
-        userValidator.validatePassword(userRegistrationRequest.getPassword().trim(), userRegistrationRequest.getPasswordCheck().trim());
+        userValidator.validate(userRegistrationRequest);
 
         Role role = roleRepository.findByRole(RoleModel.USER);
         Set<Role> roles = new HashSet<>();
         roles.add(role);
 
-        UserAccount newUserAccount = new UserAccount(userRegistrationRequest.getFirstName().trim(), userRegistrationRequest.getLastName().trim(),
-                userRegistrationRequest.getEmail().trim(), passwordEncoder.encode(userRegistrationRequest.getPassword().trim()),
-                passwordEncoder.encode(userRegistrationRequest.getPasswordCheck().trim()), RegistrationStatus.NEW, roles);
+        UserAccount newUserAccount = UserAccount.userAccountBuilder()
+                .firstName(userRegistrationRequest.getFirstName().trim())
+                .lastName(userRegistrationRequest.getLastName().trim())
+                .email(userRegistrationRequest.getEmail().trim())
+                .password(passwordEncoder.encode(userRegistrationRequest.getPassword().trim()))
+                .passwordCheck(passwordEncoder.encode(userRegistrationRequest.getPasswordCheck().trim()))
+                .registrationStatus(RegistrationStatus.NEW)
+                .roles(roles)
+                .build();
+
         userAccountRepository.save(newUserAccount);
     }
 }
