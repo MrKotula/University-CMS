240b982ae37c717593298cde9ea6ab96a3b399f5
Bohdan Bilyk <bohdanbilyk1001@gmail.com>
2023-07-31 17:22:02 +0200

    Changed type of object

--- src/main/java/ua/foxminded/university/registration/UserDetailsServiceImpl.java
+++ src/main/java/ua/foxminded/university/registration/UserDetailsServiceImpl.java
@@ -1,29 +1,26 @@
 package ua.foxminded.university.registration;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
-import ua.foxminded.university.entity.User;
+import ua.foxminded.university.entity.UserAccount;
 import ua.foxminded.university.repository.UserAccountRepository;
+import java.util.Optional;
 
 @Component
+@AllArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
     private UserAccountRepository userAccountRepository;
 
-    @Autowired
-    public UserDetailsServiceImpl(UserAccountRepository userAccountRepository) {
-        this.userAccountRepository = userAccountRepository;
-    }
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userAccountRepository.getUserByEmail(username);
+        Optional<UserAccount> user = userAccountRepository.getUserByEmail(username);
 
-        if (user == null) {
+        if (user.isEmpty()) {
             throw new UsernameNotFoundException("Could not find user!");
         }
-        return new MyUserDetails(user);
+        return new MyUserDetails(user.get());
     }
 }
