80f15be41d66e14a416c17188c3b1067adb83ddc
Bohdan Bilyk <bohdanbilyk1001@gmail.com>
2023-07-31 17:21:34 +0200

    Changed validation

--- src/main/java/ua/foxminded/university/service/impl/StudentAccountServiceImpl.java
+++ src/main/java/ua/foxminded/university/service/impl/StudentAccountServiceImpl.java
@@ -13,16 +13,18 @@ import ua.foxminded.university.repository.RoleRepository;
 import ua.foxminded.university.repository.StudentAccountRepository;
 import ua.foxminded.university.entity.StudentAccount;
 import ua.foxminded.university.entity.enums.RegistrationStatus;
+import ua.foxminded.university.validator.StudentValidator;
 import ua.foxminded.university.validator.exception.ValidationException;
 import ua.foxminded.university.service.StudentAccountService;
-import ua.foxminded.university.validator.UserValidator;
 
 @Service
 @AllArgsConstructor
 public class StudentAccountServiceImpl implements StudentAccountService {
 
-    private final UserValidator userValidator;
+    private final StudentValidator studentValidator;
+
     private final PasswordEncoder passwordEncoder;
+
     private final StudentAccountRepository studentAccountRepository;
 
     private RoleRepository roleRepository;
@@ -34,50 +36,63 @@ public class StudentAccountServiceImpl implements StudentAccountService {
 
     @Override
     public void addStudentCourse(String studentId, String courseId) {
+        studentValidator.validateStudentId(studentId);
+        studentValidator.validateCourseId(courseId);
         studentAccountRepository.addStudentCourse(studentId, courseId);
     }
 
     @Override
     public void removeStudentFromCourse(String studentId, String courseId) {
+        studentValidator.validateStudentId(studentId);
+        studentValidator.validateCourseId(courseId);
         studentAccountRepository.removeStudentFromCourse(studentId, courseId);
     }
 
     @Override
-    public void deleteById(String id) {
-        studentAccountRepository.deleteById(id);
+    public void deleteById(String studentId) {
+        studentValidator.validateStudentId(studentId);
+        studentAccountRepository.deleteById(studentId);
     }
 
     @Override
     public void register(UserRegistrationRequest userRegistrationRequest) throws ValidationException {
-        userValidator.validateLogin(userRegistrationRequest.getEmail().trim());
-        userValidator.validateData(userRegistrationRequest.getEmail().trim(), userRegistrationRequest.getFirstName().trim(), userRegistrationRequest.getLastName().trim());
-        userValidator.validatePassword(userRegistrationRequest.getPassword().trim(), userRegistrationRequest.getPasswordCheck().trim());
+        studentValidator.validateStudent(userRegistrationRequest);
 
         Role role = roleRepository.findByRole(RoleModel.STUDENT);
         Set<Role> roles = new HashSet<>();
         roles.add(role);
 
-        StudentAccount newStudentAccount = new StudentAccount(userRegistrationRequest.getFirstName().trim(), userRegistrationRequest.getLastName().trim(),
-                userRegistrationRequest.getEmail().trim(), passwordEncoder.encode(userRegistrationRequest.getPassword().trim()),
-                passwordEncoder.encode(userRegistrationRequest.getPasswordCheck().trim()), RegistrationStatus.NEW, roles);
+        StudentAccount newStudentAccount = StudentAccount.studentBuilder()
+                .firstName(userRegistrationRequest.getFirstName().trim())
+                .lastName(userRegistrationRequest.getLastName().trim())
+                .email(userRegistrationRequest.getEmail().trim())
+                .password(passwordEncoder.encode(userRegistrationRequest.getPassword().trim()))
+                .passwordCheck(passwordEncoder.encode(userRegistrationRequest.getPasswordCheck().trim()))
+                .registrationStatus(RegistrationStatus.NEW)
+                .roles(roles)
+                .build();
 
         studentAccountRepository.save(newStudentAccount);
     }
 
     @Override
     public void updateEmail(StudentAccount studentAccount) throws ValidationException {
-        userValidator.validateEmail(studentAccount.getEmail());
+        studentValidator.validateStudentId(studentAccount.getUserId());
+        studentValidator.validateEmail(studentAccount.getEmail());
 
         studentAccountRepository.save(studentAccount);
     }
 
     @Override
     public void updatePassword(StudentAccount studentAccount) {
+        studentValidator.validateStudentId(studentAccount.getUserId());
         studentAccountRepository.save(studentAccount);
     }
 
     @Override
     public void changeGroup(String groupId, String studentId) {
+        studentValidator.validateStudentId(studentId);
+        studentValidator.validateGroupId(groupId);
         studentAccountRepository.changeGroup(groupId, studentId);
     }
 }
