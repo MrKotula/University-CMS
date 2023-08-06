5e1a8fbf0d9396177275e69ebd0c4bc04745e7df
Bohdan Bilyk <bohdanbilyk1001@gmail.com>
2023-08-06 15:20:56 +0200

    Added fields

--- src/main/java/ua/foxminded/university/entity/StudentAccount.java
+++ src/main/java/ua/foxminded/university/entity/StudentAccount.java
@@ -1,7 +1,12 @@
 package ua.foxminded.university.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,6 +15,7 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import ua.foxminded.university.entity.enums.RegistrationStatus;
+import java.util.HashSet;
 import java.util.Set;
 
 @NoArgsConstructor
@@ -24,6 +30,17 @@ public class StudentAccount extends UserAccount {
     @Column(name = "group_id")
     private String groupId;
 
+    @Column(name = "student_card")
+    private String studentCard;
+
+    @ManyToMany(targetEntity = Course.class, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @Column(name = "courses")
+    @JoinTable(
+            name = "students_courses", schema = "schedule",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id"))
+    private Set<Course> courses = new HashSet<>();
+
     public StudentAccount(String userId, String firstName, String lastName, String email, String password, String passwordCheck, RegistrationStatus registrationStatus,
                           Set<Role> roles, String groupId) {
         super(userId, firstName, lastName, email, password, passwordCheck, registrationStatus, roles);
@@ -36,9 +53,15 @@ public class StudentAccount extends UserAccount {
         this.groupId = groupId;
     }
 
-
     @Builder(builderMethodName = "studentBuilder")
     public StudentAccount(String firstName, String lastName, String email, String password, String passwordCheck, RegistrationStatus registrationStatus, Set<Role> roles) {
         super(firstName, lastName, email, password, passwordCheck, registrationStatus, roles);
+    }
+
+    public StudentAccount(String userId, String firstName, String lastName, String email, String password, String passwordCheck, RegistrationStatus registrationStatus,
+                          Set<Role> roles, String groupId, String studentCard) {
+        super(userId, firstName, lastName, email, password, passwordCheck, registrationStatus, roles);
+        this.groupId = groupId;
+        this.studentCard = studentCard;
     }
 }
