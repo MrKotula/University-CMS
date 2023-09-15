7d6a6ecb4d3f7a730d7f1e78f929e151045b68dc
Bohdan Bilyk <bohdanbilyk1001@gmail.com>
2023-09-15 15:21:38 +0200

    Added new field

--- src/main/java/ua/foxminded/university/entity/Course.java
+++ src/main/java/ua/foxminded/university/entity/Course.java
@@ -1,15 +1,22 @@
 package ua.foxminded.university.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -29,8 +36,26 @@ public class Course {
     @Column(name = "course_description")
     private String courseDescription;
 
+    @OneToMany(targetEntity = TeacherAccount.class, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @Column(name = "teachers")
+    @JoinTable(
+            name = "course_teachers", schema = "schedule",
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<TeacherAccount> teachers = new ArrayList<>();
+
     public Course(String courseName, String courseDescription) {
         this.courseName = courseName;
         this.courseDescription = courseDescription;
+    }
+
+    public Course(String courseId, String courseName, String courseDescription) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.courseDescription = courseDescription;
+    }
+
+    public void addTeacher(TeacherAccount teacherAccount) {
+        this.teachers.add(teacherAccount);
     }
 }
