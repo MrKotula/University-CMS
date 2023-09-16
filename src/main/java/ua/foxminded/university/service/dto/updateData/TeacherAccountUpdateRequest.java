ba87c74b4056b1b17c78f342458f3ef3e133ec31
Bohdan Bilyk <bohdanbilyk1001@gmail.com>
2023-09-16 16:42:47 +0200

    Created dto class for TeacherAccount

---
+++ src/main/java/ua/foxminded/university/service/dto/updateData/TeacherAccountUpdateRequest.java
@@ -0,0 +1,33 @@
+package ua.foxminded.university.service.dto.updateData;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import ua.foxminded.university.entity.Role;
+import ua.foxminded.university.entity.StudentAccount;
+import ua.foxminded.university.entity.enums.Degree;
+import ua.foxminded.university.entity.enums.RegistrationStatus;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TeacherAccountUpdateRequest {
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @ToString.Exclude
+    private String password;
+    @ToString.Exclude
+    private String passwordCheck;
+
+    private Set<Role> roles;
+    private RegistrationStatus registrationStatus;
+    private Degree degree;
+    private String phoneNumber;
+    private List<StudentAccount> diplomaStudents;
+}
