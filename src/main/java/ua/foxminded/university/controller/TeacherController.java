e3495b3b33bcd6123813053ad714920d7914d2c6
Bohdan Bilyk <bohdanbilyk1001@gmail.com>
2023-09-16 16:50:40 +0200

    Created method in controller

--- src/main/java/ua/foxminded/university/controller/TeacherController.java
+++ src/main/java/ua/foxminded/university/controller/TeacherController.java
@@ -2,7 +2,11 @@ package ua.foxminded.university.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import ua.foxminded.university.service.TeacherAccountService;
 
 @Controller
@@ -10,4 +14,11 @@ import ua.foxminded.university.service.TeacherAccountService;
 @Log4j2
 public class TeacherController {
     private final TeacherAccountService teacherAccountService;
+
+    @GetMapping("/teacher")
+    public String openInfoTeacherPage(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+        model.addAttribute("teacherUpdateRequest", teacherAccountService.getTeacherByEmail(userDetails.getUsername()));
+
+        return "/teacher/teacherPanel";
+    }
 }
