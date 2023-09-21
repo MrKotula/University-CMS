f7ab2bee0691a1ec05bb56bddf11ee48b8449454
Bohdan Bilyk <bohdanbilyk1001@gmail.com>
2023-09-21 20:25:40 +0200

    Added test methods

--- src/test/java/ua/foxminded/university/service/impl/StudentAccountServiceImplTest.java
+++ src/test/java/ua/foxminded/university/service/impl/StudentAccountServiceImplTest.java
@@ -15,11 +15,15 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import ua.foxminded.university.entity.Course;
+import ua.foxminded.university.entity.Role;
+import ua.foxminded.university.entity.enums.RoleModel;
+import ua.foxminded.university.repository.RoleRepository;
 import ua.foxminded.university.service.dto.registration.UserRegistrationRequest;
 import ua.foxminded.university.repository.CourseRepository;
 import ua.foxminded.university.repository.StudentAccountRepository;
 import ua.foxminded.university.entity.StudentAccount;
 import ua.foxminded.university.entity.enums.RegistrationStatus;
+import ua.foxminded.university.service.dto.response.StudentAccountResponse;
 import ua.foxminded.university.validator.exception.ValidationException;
 import ua.foxminded.university.service.StudentAccountService;
 import java.util.Arrays;
@@ -27,6 +31,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @SpringBootTest
 @ContextConfiguration(initializers = {StudentAccountServiceImplTest.Initializer.class})
@@ -41,6 +46,9 @@ class StudentAccountServiceImplTest {
 
     @Autowired
     CourseRepository courseRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
 
     StudentAccount testStudentAccount = new StudentAccount("33c99439-aaf0-4ebd-a07a-bd0c550db4e1", "John", "Doe", "dis@ukr.net", null, null,
            RegistrationStatus.NEW, new HashSet<>(),"3c01e6f1-762e-43b8-a6e1-7cf493ce92e2", "DT94381727");
@@ -103,9 +111,12 @@ class StudentAccountServiceImplTest {
     void verifyUseMethodUChangeGroup() {
         Course courseMath = courseRepository.findById("1d95bc79-a549-4d2c-aeb5-3f929aee0f22").get();
         Course courseBiology = courseRepository.findById("1d95bc79-a549-4d2c-aeb5-3f929aee1234").get();
+
         HashSet<Course> courses = new HashSet<>(Arrays.asList(courseMath, courseBiology));
+
         testStudentAccount.setCourses(courses);
         testStudentAccount.setGroupId("3c01e6f1-762e-43b8-a6e1-7cf493ce92e2");
+
         studentAccountService.changeGroup("3c01e6f1-762e-43b8-a6e1-7cf493ce5325", "33c99439-aaf0-4ebd-a07a-bd0c550db4e1");
 
         assertEquals(Optional.of(testStudentAccount), studentAccountRepository.findById("33c99439-aaf0-4ebd-a07a-bd0c550db4e1"));
@@ -116,8 +127,11 @@ class StudentAccountServiceImplTest {
     void shouldReturnListOfStudentsWhenUseGetStudentsWithCourseName() {
         Course courseMath = courseRepository.findById("1d95bc79-a549-4d2c-aeb5-3f929aee0f22").get();
         Course courseBiology = courseRepository.findById("1d95bc79-a549-4d2c-aeb5-3f929aee1234").get();
+
         HashSet<Course> courses = new HashSet<>(Arrays.asList(courseMath, courseBiology));
+
         testStudentAccount.setCourses(courses);
+
         List<StudentAccount> testListStudentAccount = Arrays.asList(testStudentAccount);
 
         assertEquals(testListStudentAccount, studentAccountService.findByCourseName("Mathematics"));
@@ -138,12 +152,41 @@ class StudentAccountServiceImplTest {
     void verifyUseMethodWhenUseInsertSaveAndAddStudentCourse() {
         Course courseMath = courseRepository.findById("1d95bc79-a549-4d2c-aeb5-3f929aee0f22").get();
         Course courseBiology = courseRepository.findById("1d95bc79-a549-4d2c-aeb5-3f929aee1234").get();
+
         HashSet<Course> courses = new HashSet<>(Arrays.asList(courseMath, courseBiology));
+
         testStudentAccount.setCourses(courses);
+
         List<StudentAccount> testListStudentAccount = Arrays.asList(testStudentAccount);
+
         studentAccountRepository.save(new StudentAccount("3c01e6f1-762e-43b8-a6e1-7cf493ce92e2", "John", "Doe", "dis@ukr.net", "123140", "123140", RegistrationStatus.NEW, new HashSet<>()));
         studentAccountService.addStudentCourse("33c99439-aaf0-4ebd-a07a-bd0c550db4e1", "1d95bc79-a549-4d2c-aeb5-3f929aee0096");
 
         assertEquals(testListStudentAccount, studentAccountService.findByCourseName("Drawing"));
+    }
+
+    @Test
+    @Transactional
+    void verifyUseMethodWhenUseFindAllStudentsTest() {
+        assertEquals(testStudentAccount, studentAccountService.findAllStudents().get(0));
+    }
+
+    @Test
+    @Transactional
+    void shouldReturnStudentAccountResponseWhenUseFindStudentByIdTest() {
+        Course courseMath = courseRepository.findById("1d95bc79-a549-4d2c-aeb5-3f929aee0f22").get();
+        Course courseBiology = courseRepository.findById("1d95bc79-a549-4d2c-aeb5-3f929aee1234").get();
+        HashSet<Course> courses = new HashSet<>(Arrays.asList(courseMath, courseBiology));
+
+        Set<Role> roles = new HashSet<>();
+        Role roleStudent = roleRepository.findByRole(RoleModel.STUDENT);
+
+        roles.add(roleStudent);
+
+        StudentAccountResponse studentAccountResponseTest = new StudentAccountResponse("33c99439-aaf0-4ebd-a07a-bd0c550db4e1", "John", "Doe", "dis@ukr.net",
+                "$2a$10$nWD4aCZMQydDrZjAFYFwOOa7lO3cuI6b/el3ZubPoCmHQnu6YrTMS", "$2a$10$nWD4aCZMQydDrZjAFYFwOOa7lO3cuI6b/el3ZubPoCmHQnu6YrTMS",
+                roles, RegistrationStatus.NEW,"3c01e6f1-762e-43b8-a6e1-7cf493ce92e2", "DT94381727", courses);
+
+        assertEquals(studentAccountResponseTest, studentAccountService.findStudentById("33c99439-aaf0-4ebd-a07a-bd0c550db4e1"));
     }
 }
