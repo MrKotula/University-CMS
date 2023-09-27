acdcadf330fd5040f83ebe4cfddd33bc531c353e
Bohdan Bilyk <bohdanbilyk1001@gmail.com>
2023-09-27 14:40:02 +0200

    Added test containers

--- src/test/java/ua/foxminded/university/controller/LoginTest.java
+++ src/test/java/ua/foxminded/university/controller/LoginTest.java
@@ -1,10 +1,19 @@
 package ua.foxminded.university.controller;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -13,10 +22,36 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ContextConfiguration(initializers = {LoginTest.Initializer.class})
+@Testcontainers
 class LoginTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Container
+    public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15.2")
+            .withDatabaseName("integration-tests-db").withUsername("sa").withPassword("sa");
+
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues
+                    .of("spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+                            "spring.datasource.username=" + postgreSQLContainer.getUsername(),
+                            "spring.datasource.password=" + postgreSQLContainer.getPassword())
+                    .applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+
+    @BeforeAll
+    static void setUp() {
+        postgreSQLContainer.start();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        postgreSQLContainer.stop();
+    }
 
     @Test
     public void accessDeniedTest() throws Exception {
