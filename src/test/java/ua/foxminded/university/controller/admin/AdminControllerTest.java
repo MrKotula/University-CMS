96c9ab9105e59ede881de411244f5ba7eecefb9d
Bohdan Bilyk <bohdanbilyk1001@gmail.com>
2023-09-27 14:38:56 +0200

    Changed tests

--- src/test/java/ua/foxminded/university/controller/admin/AdminControllerTest.java
+++ src/test/java/ua/foxminded/university/controller/admin/AdminControllerTest.java
@@ -1,12 +1,22 @@
 package ua.foxminded.university.controller.admin;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import ua.foxminded.university.entity.enums.RegistrationStatus;
 import ua.foxminded.university.service.CourseService;
 import ua.foxminded.university.service.RoleService;
@@ -20,8 +30,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WithMockUser(value = "admin@user", roles = "ADMIN")
-@WebMvcTest(AdminController.class)
+@WithMockUser(value = "admin@user", authorities = "ADMIN")
+@SpringBootTest
+@AutoConfigureMockMvc
+@ContextConfiguration(initializers = {AdminControllerTest.Initializer.class})
+@Testcontainers
 class AdminControllerTest {
 
     @Autowired
@@ -35,6 +48,30 @@ class AdminControllerTest {
 
     @MockBean
     private CourseService courseService;
+
+    @Container
+    public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15.2")
+            .withDatabaseName("integration-tests-db").withUsername("sa").withPassword("sa");
+
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues
+                    .of("spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+                            "spring.datasource.username=" + postgreSQLContainer.getUsername(),
+                            "spring.datasource.password=" + postgreSQLContainer.getPassword())
+                    .applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+
+    @BeforeAll
+    static void setUp() {
+        postgreSQLContainer.start();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        postgreSQLContainer.stop();
+    }
 
     @Test
     public void adminPageTest() throws Exception {
@@ -99,6 +136,11 @@ class AdminControllerTest {
 
     @Test
     public void shouldUpdateUserRolesTest() throws Exception {
+        UserAccountUpdateRequest userAccountUpdateRequest = new UserAccountUpdateRequest("33c99439-aaf0-4ebd-a07a-bd0c550d2311", "Jane", "Does", "dtestMail@gmail.com",
+                "$2a$10$nWD4aCZMQydDrZjAFYFwOOa7lO3cuI6b/el3ZubPoCmHQnu6YrTMS", "$2a$10$nWD4aCZMQydDrZjAFYFwOOa7lO3cuI6b/el3ZubPoCmHQnu6YrTMS", new HashSet<>(), RegistrationStatus.NEW );
+
+        when(userAccountService.findUserById("33c99439-aaf0-4ebd-a07a-bd0c550db4e1")).thenReturn(userAccountUpdateRequest);
+
         mockMvc.perform(post("/admin/user/editRole/33c99439-aaf0-4ebd-a07a-bd0c550d2311")
                         .param("roles", "ADMIN")
                         .contentType(MediaType.APPLICATION_JSON))
